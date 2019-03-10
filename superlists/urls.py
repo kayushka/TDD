75c1b41cec5da8aa7183f04acae822e2b69d1b89
Kaja Machalska <machal.kaja@gmail.com>
2019-03-10 12:29:21 +0100

    new url, view and template to display lists w tests

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,9 +18,7 @@ from django.conf.urls import url
 from lists import views 
 
 urlpatterns = [
-	# Examples:
 	url(r'^$', views.home_page, name='home'),
-	# url(r'^blog/', include('blog.urls')),
-
+	url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 	# url(r'admin/', admin.site.urls),
 ]
