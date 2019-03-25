afaee67f2d7725112c116351b0f5a12d25d58b8f
Kaja Machalska <machal.kaja@gmail.com>
2019-03-25 19:24:28 +0100

    move sqlite database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time 
 from selenium.common.exceptions import WebDriverException
+import os
 
 MAX_WAIT = 10
 
@@ -10,7 +11,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+		self.server = self.live_server_url
+		staging_server = os.environ.get('STAGING_SERVER')
+		if staging_server:
+			self.server = 'http://' + staging_server
 
 	def tearDown(self):
 		self.browser.quit()
@@ -31,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server)
 
 		# She notices the page title and header mention to-do list
 		self.assertIn('To-Do', self.browser.title)
@@ -69,7 +73,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		
 		# Edith starts a new to-do list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
@@ -87,7 +91,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 		# Francis visits the home page. There is no sign of Edith's
 		# list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -113,7 +117,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		# Edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server)
 		self.browser.set_window_size(1024, 768)
 
 		# She notices the input box is nicely centerded
